fe888522458314ea272304d9b3a2c2c4ae08a244
Brian Chan <alto1020@gmail.com>
2016-12-22 03:49:15 -0800

    Added limited bluetooth functionality

--- app/src/main/java/com/example/brian/piano/MainActivity.java
+++ app/src/main/java/com/example/brian/piano/MainActivity.java
@@ -1,24 +1,67 @@
 package com.example.brian.piano;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
+    private BluetoothAdapter bluetoothAdapter;
+    private Button applyButton, resetButton, connectButton;
+    private Spinner soundSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        applyButton = (Button) findViewById(R.id.applyButton);
+        resetButton = (Button) findViewById(R.id.resetButton);
+        connectButton = (Button) findViewById(R.id.connectButton);
+        soundSpinner = (Spinner) findViewById(R.id.spinner);
+
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                connect(v);
+            }
+        });
+
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.planets_array, android.R.layout.simple_spinner_item);
+                R.array.sounds_array, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        soundSpinner.setAdapter(adapter);
+    }
+
+    //Turns on the bluetooth adapter for the device
+    private void turnOn(View v) {
+        bluetoothAdapter.enable();
+    }
+
+    //Turns off the bluetooth adapter for the device
+    private void turnOff(View v) {
+        bluetoothAdapter.disable();
+    }
+
+    private void connect(View v) {
+        turnOn(v);
+        startActivity(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
+        Toast.makeText(getApplicationContext(), "Select device", Toast.LENGTH_LONG).show();
+        System.err.println(bluetoothAdapter.getBondedDevices());
     }
 }
