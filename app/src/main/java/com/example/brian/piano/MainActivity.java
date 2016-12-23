c95fdf866c2465bb076261271f6a78c18ad87106
Brian Chan <alto1020@gmail.com>
2016-12-23 03:25:16 -0800

    Added toolbar

--- app/src/main/java/com/example/brian/piano/MainActivity.java
+++ app/src/main/java/com/example/brian/piano/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.brian.piano;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.provider.Settings;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -20,6 +25,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         applyButton = (Button) findViewById(R.id.applyButton);
@@ -45,8 +62,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String sound = soundSpinner.getSelectedItem().toString();
 
-                Toast.makeText(getApplicationContext(), "Piano sound set to " + sound,
-                        Toast.LENGTH_SHORT).show();
+                Snackbar.make(v, "Piano sound set to " + sound, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
 
@@ -54,8 +71,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 soundSpinner.setSelection(0);
 
-                Toast.makeText(getApplicationContext(), "Piano sound reset",
-                        Toast.LENGTH_SHORT).show();
+                Snackbar.make(v, "Piano sound reset", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                //Toast.makeText(getApplicationContext(), "Piano sound reset",
+                //        Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -77,5 +97,27 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
         Toast.makeText(getApplicationContext(), "Select device", Toast.LENGTH_LONG).show();
         System.err.println(bluetoothAdapter.getBondedDevices());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+
+        return super.onOptionsItemSelected(item);
     }
 }
