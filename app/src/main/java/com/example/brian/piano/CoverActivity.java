7883e450c1218f374d16131b624ba0ba378e5594
Brian Chan <alto1020@gmail.com>
2017-01-06 03:47:07 -0800

    Added piano key press logic

--- app/src/main/java/com/example/brian/piano/CoverActivity.java
+++ app/src/main/java/com/example/brian/piano/CoverActivity.java
@@ -1,16 +1,45 @@
 package com.example.brian.piano;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
 
 public class CoverActivity extends AppCompatActivity {
+
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cover);
+
+        Button helpButton = (Button) findViewById(R.id.aboutButton);
+
+        mp = MediaPlayer.create(this, R.raw.pianotest);
+
+        helpButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mp.start();
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    mp.stop();
+
+                    try {
+                        mp.prepare();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return true;
+            }
+        });
     }
 
     //Send to tone selection screen
@@ -20,6 +49,6 @@ public class CoverActivity extends AppCompatActivity {
     }
 
     //How to use fragment
-    public void help(View v) { }
-
+    public void help(View v) {
+    }
 }
---
+++ app/src/main/java/com/example/brian/piano/Piano.java
@@ -0,0 +1,57 @@
+package com.example.brian.piano;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+/**
+ * Created by Brian Chan on 1/6/2017.
+ */
+
+public class Piano {
+
+    private final int numKeys = 39;
+    private int prevState[] = new int[numKeys];
+    private int currState[] = new int[numKeys];
+    private MediaPlayer notes[] = new MediaPlayer[numKeys];
+
+    public Piano() {}
+
+    //Parses new string
+    public void parseString(String input) {
+        prevState = currState;
+
+        for (int i = 0; i < input.length(); i++) {
+            currState[i] = Character.getNumericValue(input.charAt(i));
+        }
+    }
+
+    //Checks if the key should change states
+    public void checkKey(int key) throws IOException {
+        if (prevState[key] == 0 && currState[key] == 1 ) {
+            playNote(key);
+        }
+        else if (prevState[key] == 1 && currState[key] == 0) {
+            if (notes[key].isPlaying()) {
+                stopNote(key);
+            }
+        }
+    }
+
+    //Inserts MediaPlayers for all the keys, has to be done in activity
+    public void insertMP(int key, MediaPlayer mp) {
+        notes[key] = mp;
+    }
+
+    public void playNote(int key) {
+        //error but works, needs to copy to activity
+        notes[key].start();
+    }
+
+    public void stopNote(int key) throws IOException {
+        notes[key].stop();
+        notes[key].prepare();
+    }
+}
